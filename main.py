1e3663aad859fa61e31fc4e85f4d095d0aaed444
Renethan <ethan.rene83560@gmail.com>
2026-04-07 15:51:58 +0200

    classe joueur

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import pygame as py
 from constantes import *
+from classes import *
 
 py.init()
 
 screen = py.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+
+
+joueur = Joueur((100,100))
+
 
 running = True
 clock = py.time.Clock()
@@ -13,8 +18,12 @@ while running:
         if event.type == py.QUIT:
             running = False
     
+    screen.fill("cyan")
 
     # BOUCLE MAIN #
+    
+    py.draw.rect(screen,"red",py.Rect(joueur.getXY(),(100,100)))
+
 
 
     py.display.flip()
