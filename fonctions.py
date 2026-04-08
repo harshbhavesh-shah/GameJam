29a56b37a21dd4671988dc6826cd3a655caeb83d
Renethan <ethan.rene83560@gmail.com>
2026-04-08 12:14:56 +0200

    texture porte test

--- fonctions.py
+++ fonctions.py
@@ -126,5 +126,4 @@ def telePorte(portes:list[Porte], j:Joueur, keys):
 
 def background(ecran:py.Surface,zone):
     match zone:
-        case "foret":
-            ecran.blits(((bg_foret_1,(0,0)),(bg_foret_2,(0,0)),(bg_foret_3,(0,0))))
+        case "foret": ecran.blits(((bg_foret_1,(0,0)),(bg_foret_2,(0,0)),(bg_foret_3,(0,0))))
--- textures.py
+++ textures.py
@@ -10,3 +10,8 @@ bg_foret_3 = py.image.load("./assets/textures/foret/background/bg3.png")
 bg_foret_1 = py.transform.scale(bg_foret_1,(SCREEN_WIDTH,SCREEN_HEIGHT))
 bg_foret_2 = py.transform.scale(bg_foret_2,(SCREEN_WIDTH,SCREEN_HEIGHT))
 bg_foret_3 = py.transform.scale(bg_foret_3,(SCREEN_WIDTH,SCREEN_HEIGHT))
+
+
+### OBJETS ###
+
+sprite_porte = py.image.load("./assets/textures/autre/porte.png")
